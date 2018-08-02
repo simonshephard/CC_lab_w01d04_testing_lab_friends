fa8413eb949b9f622359b4c1a77dcec8edbe9357
Simon Shephard <simonshephard@hotmail.com>
2018-08-02 15:49:38 +0100

    initial tests and functions completed

--- friends.rb
+++ friends.rb
@@ -1,3 +1,49 @@
 def get_name(person)
   return person[:name]
 end
+
+def get_tv_show(person)
+  return person[:favourites][:tv_show]
+end
+
+def likes_to_eat(person, food)
+  array = person[:favourites][:snacks]
+  return array.include?(food)
+end
+
+def add_friend(person, friend)
+  person[:friends].push(friend)
+end
+
+def remove_friend(person, friend)
+  person[:friends].delete(friend)
+end
+
+def total_money(people)
+  total = 0
+  for person in people
+    total += person[:monies]
+  end
+  return total
+end
+
+def loan_money(person1, person2, money)
+  person1[:monies] -= money
+  person2[:monies] += money
+end
+
+def set_of_foods(people)
+  foods = []
+  for person in people
+    foods.concat(person[:favourites][:snacks])
+  end
+  return foods
+end
+
+def find_people_with_no_friends(people)
+  people_with_no_friends = []
+  for person in people
+    people_with_no_friends.push(person[:name]) if person[:friends].length == 0
+  end
+  return people_with_no_friends
+end
--- specs/friends_spec.rb
+++ specs/friends_spec.rb
@@ -73,34 +73,91 @@ class TestFriends < MiniTest::Test
 
   # 2. For a given person, return their favourite tv show
   # (e.g. the function favourite_tv_show(@person2) should return the string "Baywatch")
+  def test_getting_tv_show
+    result = get_tv_show(@person2)
+    assert_equal("Baywatch", result)
+  end
 
 
   # 3. For a given person, check if they like a particular food
   # (e.g. the function likes_to_eat(@person2, "bread") should return true, likes_to_eat(@person3, "spinach") should return false)
+  def test_check_food
+    result = likes_to_eat(@person2, "bread")
+    assert_equal(true, result)
+  end
+
 
   # 4. For a given person, add a new name to their list of friends
   # (e.g. the function add_friend(@person2, "Scrappy-Doo") should add Scrappy-Doo to the friends.)
   # (hint: This function should not return anything. After the function call, check for the length of the friends array to test it!)
+  def test_check_add_friend
+    length_before = @person2[:friends].length
+    expected = length_before + 1
+    add_friend(@person2, "Scrappy-Doo")
+    result = @person2[:friends].length
+    assert_equal(expected, result)
+  end
 
 
   # 5. For a given person, remove a specific name from their list of friends
   # (hint: Same as above, testing for the length of the array should be sufficient)
+  def test_remove_friend
+    length_before = @person4[:friends].length
+    expected = length_before - 1 #ok to use functions or should we use fixed values here???
+    remove_friend(@person4, "Daphne")
+    result = @person4[:friends].length
+    assert_equal(expected, result)
+  end
+
 
 
   # 6. Find the total of everyone's money
   # (hint: use the @people array, remember how we checked the total number of eggs yesterday?)
+  def test_total_money
+    result = total_money(@people)
+    assert_equal(143, result)
+  end
+
 
 
   # 7. For two given people, allow the first person to loan a given value of money to the other
   # (hint: our function will probably need 3 arguments passed to it... the lender, the lendee, and the amount for this function)
   # (hint2: You should test if both the lender's and the lendee's money have changed, maybe two assertions?)
+  def test_loan_money
+    loan_money(@person5, @person4, 50)
+    result1 = @person5[:monies]
+    result2 = @person4[:monies]
+    assert_equal(50, result1)
+    assert_equal(70, result2)
+  end
+
+
 
 
   # 8. Find the set of everyone's favourite food joined together
   # (hint: concatenate the favourites/snack arrays together)
+  def test_set_of_foods
+    expected = ["charcuterie", "soup", "bread", "Scooby snacks", "spaghetti", "ratatouille", "spinach"]
+    result = set_of_foods(@people)
+    assert_equal(expected, result)
+  end
+
+
+
 
 
   # 9. Find people with no friends
   # (hint: return an array, there might be more people in the future with no friends!)
+  def test_find_people_with_no_friends
+    expected = ["Daphne"]
+    result = find_people_with_no_friends(@people)
+    assert_equal(expected, result)
+  end
+
+
+
+
+
+
 
 end
