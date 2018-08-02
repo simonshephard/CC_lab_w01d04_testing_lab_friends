e418ec8faa44025b419ee302a2598980e5975faf
Simon Shephard <simonshephard@hotmail.com>
2018-08-02 14:36:34 +0100

    initial commit

---
+++ file.rb
@@ -0,0 +1,19 @@
+require("minitest/autorun")
+require("minitest/rg")
+
+class TestExample < MiniTest::Test
+
+  def setup()
+    @person1 = {"name" => "Alex", "age" => 30}
+  end
+
+  def test_can_change_name
+    @person1["name"] = "Alexander"
+    assert_equal("Alexander", @person1["name"])
+  end
+
+  def test_person_has_name
+    assert_equal("Alex", @person1["name"])
+  end
+
+end
